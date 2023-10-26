00ff06a0399b89984a809e3d50b27134f40d632b
Yousef ZinsazK78 <yousefzinsazk78@gmail.com>
2023-10-26 22:26:40 +0330

    Delete godotenv commented in api.go

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -10,10 +10,6 @@ func main() {
 	port := flag.String("port", "8001", "you can set new port into server")
 	log.Println("server has started on localhost:" + *port)
 
-	// if err := godotenv.Load("./internal/config/.env"); err != nil {
-	// 	log.Fatal(err)
-	// }
-
 	router := routes.Init()
 
 	if err := router.Run("localhost:" + *port); err != nil {
